7bf954f679b8525884f38f33873c11791920db20
JWhiteCat <wanjian@njust.edu.cn>
2023-08-13 21:32:48 +0800

    更换绘制三角形算法

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <iostream>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -79,42 +80,39 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
     }
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+Vec3f barycentric(Vec2i *pts, Vec2i P)
 {
-    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
-    if (t0.y > t1.y)
-        std::swap(t0, t1);
-    if (t0.y > t2.y)
-        std::swap(t0, t2);
-    if (t1.y > t2.y)
-        std::swap(t1, t2);
-    int total_height = t2.y - t0.y;
-    for (int y = t0.y; y <= t1.y; y++)
+    Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u.z) < 1)
+        return Vec3f(-1, 1, 1);
+    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color)
+{
+    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+    for (int i = 0; i < 3; i++)
     {
-        int segment_height = t1.y - t0.y + 1;
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t0.y) / segment_height; // be careful with divisions by zero
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = t0 + (t1 - t0) * beta;
-        if (A.x > B.x)
-            std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++)
-        {
-            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
-        }
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
     }
-    for (int y = t1.y; y <= t2.y; y++)
+    Vec2i P;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
     {
-        int segment_height = t2.y - t1.y + 1;
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t1.y) / segment_height; // be careful with divisions by zero
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = t1 + (t2 - t1) * beta;
-        if (A.x > B.x)
-            std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++)
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
         {
-            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
+            Vec3f bc_screen = barycentric(pts, P);
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0)
+                continue;
+            image.set(P.x, P.y, color);
         }
     }
 }
@@ -129,7 +127,7 @@ int main(int argc, char **argv)
     // {
     //     model = new Model("obj/african_head.obj");
     // }
-    TGAImage image(width, height, TGAImage::RGB);
+    // TGAImage image(width, height, TGAImage::RGB);
     // // line(13, 20, 80, 40, image, white);
     // for (int i = 0; i < model->nfaces(); i++)
     // {
@@ -146,15 +144,19 @@ int main(int argc, char **argv)
     //     }
     // }
 
-    Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    // Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+    // Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
 
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+    // triangle(t0[0], t0[1], t0[2], image, red);
+    // triangle(t1[0], t1[1], t1[2], image, white);
+    // triangle(t2[0], t2[1], t2[2], image, green);
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
+    TGAImage frame(200, 200, TGAImage::RGB);
+    Vec2i pts[3] = {Vec2i(10, 10), Vec2i(100, 30), Vec2i(190, 160)};
+    triangle(pts, frame, TGAColor(255, 0, 0));
+
+    frame.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    frame.write_tga_file("output.tga");
     return 0;
 }
