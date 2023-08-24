d43861d38d459606aa4c12743005720005f00db8
JWhiteCat <wanjian@njust.edu.cn>
2023-08-24 11:45:26 +0800

    法线贴图

--- main.cpp
+++ main.cpp
@@ -17,21 +17,28 @@ Vec3f up(0, 1, 0);
 
 struct GouraudShader : public IShader
 {
-    Vec3f varying_intensity; // written by vertex shader, read by fragment shader
+    Vec3f varying_intensity;      // written by vertex shader, read by fragment shader
+    mat<2, 3, float> varying_uv;  // same as above
+    mat<4, 4, float> uniform_M;   //  Projection*ModelView
+    mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
 
     virtual Vec4f vertex(int iface, int nthvert)
     {
-        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));                               // read the vertex from .obj file
-        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;                             // transform it to screen coordinates
-        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir); // get diffuse lighting intensity
-        return gl_Vertex;
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        // varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir); // get diffuse lighting intensity
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        return Viewport * Projection * ModelView * gl_Vertex;    // transform it to screen coordinates
     }
 
     virtual bool fragment(Vec3f bar, TGAColor &color)
     {
-        float intensity = varying_intensity * bar;   // interpolate intensity for the current pixel
-        color = TGAColor(255, 255, 255) * intensity; // well duh
-        return false;                                // no, we do not discard this pixel
+        // float intensity = varying_intensity * bar; // interpolate intensity for the current pixel
+        Vec2f uv = varying_uv * bar; // interpolate uv for the current pixel
+        Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+        Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+        float intensity = std::max(0.f, n * l);
+        color = model->diffuse(uv) * intensity; // well duh
+        return false;                           // no, we do not discard this pixel
     }
 };
 
@@ -55,6 +62,8 @@ int main(int argc, char **argv)
     TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
     GouraudShader shader;
+    shader.uniform_M = Projection * ModelView;
+    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
     for (int i = 0; i < model->nfaces(); i++)
     {
         Vec4f screen_coords[3];
