dc922e210ea2f72e9281ac97b41b2fd1785921a4
JWhiteCat <wanjian@njust.edu.cn>
2023-08-24 16:06:13 +0800

    高光

--- main.cpp
+++ main.cpp
@@ -36,9 +36,14 @@ struct GouraudShader : public IShader
         Vec2f uv = varying_uv * bar; // interpolate uv for the current pixel
         Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
         Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
-        float intensity = std::max(0.f, n * l);
-        color = model->diffuse(uv) * intensity; // well duh
-        return false;                           // no, we do not discard this pixel
+        Vec3f r = (n * (n * l * 2.f) - l).normalize(); // reflected light
+        float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
+        float diff = std::max(0.f, n * l);
+        TGAColor c = model->diffuse(uv);
+        color = c;
+        for (int i = 0; i < 3; i++)
+            color[i] = std::min<float>(5 + c[i] * (diff + .6 * spec), 255);
+        return false; // no, we do not discard this pixel
     }
 };
 
